60e46b6f7682c97e6f615aae3489087e6e5d0b5e
joonsung.um <joonsung.um@gmail.com>
2021-11-09 21:23:20 +0900

    add new map

---
+++ apps/client/components/NewMap.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react'
+import lottie from 'lottie-web'
+import { Box } from '@mui/material'
+
+const NewMap = () => {
+  //lottie
+  const likecontainer = useRef()
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: likecontainer.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./data.json'),
+    })
+  }, [])
+  return (
+    <Box>
+      <Box
+        ref={likecontainer}
+        sx={{
+          width: '100%',
+          height: '100%',
+          margin: '0 auto',
+          position: 'absolute',
+          zIndex: 1000,
+        }}
+      ></Box>
+    </Box>
+  )
+}
+
+export default NewMap
