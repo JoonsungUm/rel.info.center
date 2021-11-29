5ec66f4ad100b77d4fc9561a684680f6c32a51ff
Joonsung Um <joonsung.um@gmail.com>
2021-11-30 02:28:16 +0900

    update map text

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,10 +1,10 @@
-import React, { Fragment, useState } from 'react'
+import React, { VFC, Fragment, useState } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import namhaePic from '../public/images/namhae.png'
 
-import { Toolbar, Box, Container, Paper, Stack, Typography } from '@mui/material'
+import { Toolbar, Box, Container, Paper, Stack, Grid, Typography } from '@mui/material'
 
 import Appbar from '../components/Appbar'
 import CenterGrid from '../components/CenterGrid'
@@ -34,7 +34,43 @@ const Home: NextPage = () => {
           <Toolbar />
 
           <Box sx={{ backgroundColor: 'namhae.main' }}>
-            <Image src={namhaePic} alt="namhae map" />
+            <Box
+              sx={{ p: { xs: 3, sm: 6 }, position: { sm: 'relative', md: 'absolute' }, color: 'whitesmoke' }}
+            >
+              <Stack
+                direction={{ sm: 'row', md: 'column' }}
+                justifyContent={{ xs: 'center', sm: 'center', md: 'left', lg: 'left' }}
+              >
+                <MapTitleTypo title="남해" />
+                <MapTitleTypo title="관계" />
+                <MapTitleTypo title="지도" />
+              </Stack>
+
+              <Box
+                sx={{
+                  color: 'primary.main',
+                  pt: { xs: 3, sm: 6 },
+                }}
+              >
+                <Grid container>
+                  <MapItemTypo title="OO스튜디오" />
+                  <MapItemTypo title="OO공방" />
+                  <MapItemTypo title="OOO빵집" />
+                  <MapItemTypo title="OOO디자인" />
+                  <MapItemTypo title="OO라이프" />
+                  <MapItemTypo title="OO클럽" />
+                  <MapItemTypo title="살러" />
+                  <MapItemTypo title="OO청년단체" />
+                  <MapItemTypo title="OO모임" />
+                  <MapItemTypo title="사진관" />
+                  <MapItemTypo title="까페" />
+                  <MapItemTypo title="..." />
+                </Grid>
+              </Box>
+            </Box>
+            <Box>
+              <Image src={namhaePic} alt="namhae map" />
+            </Box>
           </Box>
 
           <Container>
@@ -87,3 +123,36 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+interface MapTitleTypoPops {
+  title: string
+}
+
+const MapTitleTypo: VFC<MapTitleTypoPops> = ({ title }) => (
+  <Typography
+    sx={{ fontSize: { xs: '2rem', sm: '3rem', md: '2.5rem', lg: '3rem' } }}
+    variant="h3"
+    component="div"
+    gutterBottom
+  >
+    {title}
+  </Typography>
+)
+
+interface MapItemTypoPops {
+  title: string
+}
+
+const MapItemTypo: VFC<MapItemTypoPops> = ({ title }) => (
+  <Grid item xs={12} sm={6} md={12}>
+    <Typography
+      sx={{
+        textAlign: { xs: 'center', sm: 'center', md: 'left', lg: 'left' },
+        fontSize: { xs: '1rem', sm: '1rem', md: '1rem', lg: '1.5rem' },
+      }}
+      gutterBottom
+    >
+      {title}
+    </Typography>
+  </Grid>
+)
